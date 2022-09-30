91d1ddb38eed8a84483e8d83c997f3d0332ffc3d
Banou <me@banou.dev>
2022-09-30 04:42:15 +0200

    refactor: ♻️improve types

--- src/parser.ts
+++ src/parser.ts
@@ -2,12 +2,15 @@ import {
   str, sequenceOf, choice, char, many, many1,
   everyCharUntil, anythingExcept, endOfInput,
   anyChar, letters, between, anyCharExcept,
-  namedSequenceOf, digits, withData, getData, whitespace, digit, mapTo, pipeParsers, Err, 
+  namedSequenceOf, digits, withData, getData, whitespace, digit, mapTo, pipeParsers, Err, Ok, Parser, 
 } from 'arcsecond'
+import { pipe } from 'fp-ts/lib/function'
+import { NonEmptyArray, groupBy, map } from 'fp-ts/lib/NonEmptyArray'
+import { toEntries } from 'fp-ts/lib/Record'
 import { AUDIO_CODECS, AUDIO_TERMS, RESOLUTIONS, VIDEO_CODECS, NORMALIZED_LANGUAGES, VIDEO_TERMS, TYPE_TERMS } from './common'
 import { istr, ichar, nonOptionalWhitespace } from './utils'
 
-type RegroupStringPrimitive = string | { [key: string]: any }
+type RegroupStringPrimitive = string | number | { [key: string]: any }
 type RegroupStringPart<S extends RegroupStringPrimitive> = S | S[]
 export type RegroupString = RegroupStringPart<RegroupStringPart<RegroupStringPrimitive>>
 
@@ -43,28 +46,31 @@ const metadataDelimiters = {
  } as const
 
 type Delimiter = keyof typeof metadataDelimiters
+type CorrespondDelimiter<T extends Delimiter> = typeof metadataDelimiters[T]
 
 const delimitersArray = Object.keys(metadataDelimiters) as Delimiter[]
 
 const getCorrespondingDelimiter = <T extends Delimiter>(delimiter: T) => metadataDelimiters[delimiter]
 
-const subtitleTerms = ['multiple subtitle', 'subtitle']
+const subtitleTerms = ['multiple subtitle', 'subtitle'] as const
 const subtitleTermToken = choice (
   subtitleTerms
     .map(istr)
-)
+) as Parser<typeof subtitleTerms[number]>
 
-const resolutionNumberToken = choice (RESOLUTIONS.map(res => str(res.toString())))
+const resolutionNumberToken = choice (
+  RESOLUTIONS.map(res => str(res.toString()))
+) as Parser<`${typeof RESOLUTIONS[number]}`>
 
 const resolutionTokenPrimitive =
   choice ([
     sequenceOf ([
       resolutionNumberToken,
-      str ('px')
+      istr ('px') as Parser<'px'>
     ]),
     sequenceOf ([
       resolutionNumberToken,
-      ichar ('p')
+      ichar ('p') as Parser<'p'>
     ]),
     resolutionNumberToken
   ])
@@ -74,7 +80,7 @@ const resolutionToken =
     resolutionTokenPrimitive,
     sequenceOf ([
       resolutionTokenPrimitive,
-      ichar ('x'),
+      ichar ('x') as Parser<'p'>,
       resolutionTokenPrimitive
     ])
   ])
@@ -83,39 +89,38 @@ const videoCodecToken =
   choice (
     VIDEO_CODECS
       .map(str)
-  )
+  ) as Parser<typeof VIDEO_CODECS[number]>
 
 const videoTermToken =
   choice (
     VIDEO_TERMS
       .map(str)
-  )
+  ) as Parser<typeof VIDEO_TERMS[number]>
 
-  // (TYPE_TERMS as Mutable<typeof TYPE_TERMS>)
 const typeTermToken =
   choice (
     TYPE_TERMS
       .map(str)
-  )
+  ) as Parser<typeof TYPE_TERMS[number]>
 
 const audioCodecToken =
   choice (
     AUDIO_CODECS
       .map(str)
-  )
+  ) as Parser<typeof AUDIO_CODECS[number]>
 
 const audioTermToken =
   choice (
     AUDIO_TERMS
       .map(str)
-  )
+  ) as Parser<typeof AUDIO_TERMS[number]>
 
 const languageToken =
   choice (
     Object
       .keys(NORMALIZED_LANGUAGES)
       .map(str)
-  )
+  ) as Parser<keyof typeof NORMALIZED_LANGUAGES>
 
 const yearToken = sequenceOf([
   digit,
@@ -128,6 +133,15 @@ const datePartToken = sequenceOf([
   digit,
   digit
 ])
+
+const test = choice([
+  yearToken,
+  datePartToken
+])
+
+const res = test.run('10')
+
+type ExtractParserResult<T extends Parser<any>> = Extract<ReturnType<T['run']>, Ok<any, any>>['result']
 
 const dateToken = choice([
   sequenceOf([
@@ -159,55 +173,43 @@ type tokenNames =
 
 const metadataTokenValue = (delimiter: Delimiter) =>
   choice ([
-    namedSequenceOf ([
-      ['typeTerm', typeTermToken]
-    ]),
-    namedSequenceOf ([
-      ['audioCodec', audioCodecToken]
-    ]),
-    namedSequenceOf ([
-      ['audioTerm', audioTermToken]
-    ]),
-    namedSequenceOf ([
-      ['videoCodec', videoCodecToken]
-    ]),
-    namedSequenceOf ([
-      ['videoTerm', videoTermToken]
-    ]),
-    namedSequenceOf ([
-      ['resolution', resolutionToken]
-    ]),
-    namedSequenceOf ([
-      ['language', languageToken]
-    ]),
-    namedSequenceOf ([
-      ['subtitleTerm', subtitleTermToken]
-    ]),
+    typeTermToken.map(res => ({ typeTerm: res })),
+    audioCodecToken.map(res => ({ audioCodec: res })),
+    audioTermToken.map(res => ({ audioTerm: res })),
+    videoCodecToken.map(res => ({ videoCodec: res })),
+    videoTermToken.map(res => ({ videoTerm: res })),
+    resolutionToken.map(res => ({ resolution: res })),
+    languageToken.map(res => ({ language: res })),
+    subtitleTermToken.map(res => ({ subtitleTerm: res })),
     // needs to be furthest down as it can override other tokens like resolutions
-    namedSequenceOf ([
-      ['date', dateToken]
-    ]),
+    dateToken.map(res => ({ date: res })),
     whitespace,
     anyCharExcept (char (getCorrespondingDelimiter(delimiter)))
   ])
 
 
-const makeMetadataToken = (delimiter: Delimiter) =>
-  pipeParsers([
-    sequenceOf ([
-      char (delimiter),
-      many (metadataTokenValue (delimiter)),
-      char (getCorrespondingDelimiter(delimiter))
-    ]),
-    mapTo(result => ({
-      type: 'METADATA' as const,
-      delimiter,
-      result: regroupStrings(result).slice(1, -1).flat()
-    }))
-  ])
+const makeMetadataToken = <T extends Delimiter>(delimiter: T) =>
+  sequenceOf ([
+    char (delimiter) as Parser<Delimiter>,
+    many (metadataTokenValue (delimiter)),
+    char (getCorrespondingDelimiter(delimiter)) as Parser<CorrespondDelimiter<Delimiter>>
+  ]).map((result) => ({
+    type: 'METADATA',
+    delimiter,
+    result: regroupStrings(result).slice(1, -1).flat()
+  }) as const)
+
+  // : [T, ExtractParserResult<ReturnType<typeof metadataTokenValue>>, CorrespondDelimiter<T>]
 
 
-const metadataToken: ReturnType<typeof makeMetadataToken> = choice (delimitersArray.map(makeMetadataToken))
+const metadataToken =
+  choice (
+    delimitersArray
+      .map(makeMetadataToken)
+  ) as Parser<ExtractParserResult<ReturnType<typeof makeMetadataToken>>>
+
+// const metadataToken: ReturnType<typeof makeMetadataToken> =
+//   choice (delimitersArray.map(makeMetadataToken))
 
 
 // console.log(
@@ -220,25 +222,23 @@ const metadataToken: ReturnType<typeof makeMetadataToken> = choice (delimitersAr
 // )
 
 const nonMetadataToken =
-  pipeParsers([
-    everyCharUntil ( choice ([metadataToken, endOfInput])),
-    mapTo(result => ({
+  everyCharUntil ( choice ([metadataToken, endOfInput]))
+    .map(result => ({
       type: 'DATA' as const,
       result: result as string
     }))
-  ])
 
 const token = choice ([
   metadataToken,
   nonMetadataToken
 ])
 
-type TokenType = Exclude<ReturnType<typeof token['run']>, Err<string, any>>['result']
+type Token = ExtractParserResult<typeof token>
+
 
 const parser =
-  pipeParsers([
-    many1 (token),
-    mapTo((result: TokenType[]) => {
+  many1 (token)
+    .map((result) => {
       const [_firstToken, ...restTokens] = result
       const firstToken =
         (_firstToken.type === 'METADATA'
@@ -246,15 +246,52 @@ const parser =
         && typeof _firstToken.result[0] === 'string')
           ? { ..._firstToken, result: [{ group: _firstToken.result[0] }] }
           : _firstToken
+
+      const tokens = [firstToken, ...restTokens]
+
+      console.log('tokens', tokens)
+
+      type foo = Extract<typeof tokens[number], { type: 'METADATA' }>[][number]['result']
+
+      const metadataTokens = 
+        tokens
+          .filter(({ type }) => type === 'METADATA') as Extract<typeof tokens[number], { type: 'METADATA' }>[]
+
+      // console.log('tokens', tokens)
+
+      const parsedMetadata =
+        metadataTokens
+          // @ts-ignore
+          .flatMap(({ result }) => result)
+          .filter(result =>
+            result
+            && typeof result === 'object'
+            && !Array.isArray(result)
+          )
+        
+
+      console.log('parsedMetadata', parsedMetadata)
+
+
+      // const metadataEntries =
+      //   pipe(
+      //     parsedMetadata,
+      //     groupBy(token => Object.keys(token)[0]),
+      //     toEntries,
+      // ) as unknown as NonEmptyArray<[string, Extract<TokenType['result'], object>]>
+
+      // const metadata = pipe(
+      //   metadataEntries,
+      //   map(([key, group]) => group.map(token => token[key]))
+      // )
+
+      // console.log('metadata', metadata)
+      
       return ({
-        // metadata: result.metadata,
-        result: [
-          firstToken,
-          ...restTokens
-        ]
+        // metadata,
+        result: tokens
       })
     })
-  ])
 
 
 // const getNamedTokens =
@@ -270,13 +307,13 @@ export const parse = (str: string) => {
   const result = parser.run(str)
   if (result.isError) throw new Error('Parser errored')
 
-  console.log(
-    JSON.stringify(
-      result.result,
-      undefined,
-      2
-    )
-  )
+  // console.log(
+  //   JSON.stringify(
+  //     result.result,
+  //     undefined,
+  //     2
+  //   )
+  // )
 
   const videoCodec =
     result
@@ -289,7 +326,7 @@ export const parse = (str: string) => {
     videoCodec
   }
 
-  console.log(res)
+  // console.log(res)
   return res
 }
 
