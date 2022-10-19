a41fbee6a940bb156262d3735a772623e56e6478
Banou <me@banou.dev>
2022-10-19 05:39:42 +0200

    fix: 🐛multi metadata tag

--- src/parser.ts
+++ src/parser.ts
@@ -279,18 +279,6 @@ const metadataTokenValue = [
   dateToken.map(res => ({ type: 'dates' as const, value: res }))
 ] as const
 
-const delimitedMetadataTokenValue = (delimiter: Delimiter) =>
-  // sequenceOf ([
-    // many (whitespace),
-    (choice as typeof _choice) ([
-      ...metadataTokenValue,
-      whitespace,
-      anyCharExcept (char (getCorrespondingDelimiter(delimiter)))
-    ])
-    // lookAhead (many (whitespace)),
-    // lookAhead (many1 ( choice ([whitespace, char (getCorrespondingDelimiter(delimiter))])))
-  // ]).map(token => token[0])
-
 const makeDelimitedMetadataToken = <T extends Delimiter>(delimiter: T) =>
   sequenceOf ([
     char (delimiter) as Parser<Delimiter>,
@@ -306,46 +294,22 @@ const makeDelimitedMetadataToken = <T extends Delimiter>(delimiter: T) =>
                   ...metadataTokenValue,
                   anyCharExcept (char (getCorrespondingDelimiter(delimiter)))
                 ]),
-                choice ([
-                  many1 (whitespace),
-                  char (getCorrespondingDelimiter(delimiter))
-                ])
-              ])
+                lookAhead (
+                  choice ([
+                    many1 (whitespace),
+                    char (getCorrespondingDelimiter(delimiter))
+                  ])
+                )
+              ]).map(result => result[1])
             ),
             anyCharExcept (char (getCorrespondingDelimiter(delimiter)))
           ]),
-        ),
-      ]),
-      // sequenceOf([
-      //   (choice as typeof _choice) ([...metadataTokenValue]),
-      //   many (
-      //     choice([
-      //       many1 (
-      //         sequenceOf([
-      //           lookAhead ( many1 (whitespace)),
-      //           (choice as typeof _choice) ([
-      //             ...metadataTokenValue,
-      //             anyCharExcept (char (getCorrespondingDelimiter(delimiter)))
-      //           ]),
-      //           lookAhead (
-      //             choice ([
-      //               many1 (whitespace),
-      //               char (getCorrespondingDelimiter(delimiter))
-      //             ])
-      //           )
-      //         ])
-      //       ),
-      //       anyCharExcept (char (getCorrespondingDelimiter(delimiter)))
-      //     ]),
-      //   ),
-      // ]),
-      // many ((choice as typeof _choice) ([...metadataTokenValue]))
-      // many (delimitedMetadataTokenValue (delimiter)),
+        ).map(result => result[0]),
+      ]).map(result => result.flat()),
       many (anyCharExcept (char (getCorrespondingDelimiter(delimiter))))
     ]),
-    // many (delimitedMetadataTokenValue (delimiter)),
     char (getCorrespondingDelimiter(delimiter)) as Parser<CorrespondDelimiter<Delimiter>>
-  ]).map((result) => console.log(result) || ({
+  ]).map((result) => ({
     type: 'METADATA' as const,
     delimiter,
     value: regroupStrings(result).slice(1, -1).flat()
@@ -575,6 +539,7 @@ export default parse
 
 // const res = parse('[DKB] Cyberpunk Edgerunners - Season 01 [1080p][HEVC x265 10bit][Dual-Audio][Multi-Subs][batch]')
 
+// const res = parse('[EMBER] Cyberpunk: Edgerunners (2022) (Season 1) [WEBRip] [1080p Dual Audio HEVC 10 bits] (Cyberpunk Edgerunners) (Batch)')
 const res = parse('[EMBER] Cyberpunk: Edgerunners (2022) (Season 1) [WEBRip] [1080p Dual Audio HEVC 10 bits] (Cyberpunk Edgerunners) (Batch)')
 console.log(res)
 
