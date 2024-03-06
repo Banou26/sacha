62768e837d89b442e3ccfb47c074a0fbc304d56f
Banou <me@banou.dev>
2024-03-06 22:54:11 +0100

    del: 🔥remove useless comment

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-// import type { AnitomyResult } from 'anitomyscript'
-// import { LanguageTag } from '../../scannarr/src/utils/language'
-// import './parser'
-
-// import anitomy from 'anitomyscript/dist/anitomyscript.bundle'
-
-// export const parse = async (): Media => {
-//   const anitomyResult: AnitomyResult = await anitomy('foo bar')
-
-
-
-//   return {
-//     checksum_algorithm: '',
-//     checksum: ''
-//   }
-// }
-
 export * from './parser'
